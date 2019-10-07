cd52002f244b02bafa9af525466344a59fd0c123
Benas Nekrošius <benasnek@gmail.com>
2019-10-07 16:23:36 +0300

    Add react client

---
+++ GameServer/Models/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using GameServer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameServer.Models
+{
+    public class Enemy
+    {
+
+        public int Health;
+        public float MoveSpeed;
+        public MoveType MoveType;
+
+    }
+}
