30709c4aaf0baebedce796a363db152724342056
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2019-10-08 22:06:31 +0300

    Add Singleton Logger class

---
+++ GameServer/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    public class Logger : ILogger
+    {
+        private const string LOG_FILE = "";
+        private static Lazy<Logger> INSTANCE = null;
+        private static object threadLock = new object();
+        private static StreamWriter output;
+
+        private Logger()
+        {
+            output = new StreamWriter(LOG_FILE);
+        }
+
+        public static Lazy<Logger> GetInstance()
+        {
+            lock (threadLock)
+            {
+                if (INSTANCE == null)
+                {
+                    INSTANCE = new Lazy<Logger>();
+                }
+            }
+            return INSTANCE;
+        }
+
+        public void Log()
+        {
+            output.WriteLine();
+        }
+    }
+}
